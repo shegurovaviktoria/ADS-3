3699a70a4826911d9af256707af728933dda43bd
viktoria shegurova <137227112+shegurovaviktoria@users.noreply.github.com>
2024-03-22 22:54:56 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 // Copyright 2022 NNTU-CS
 #include "alg.h"
+#include <iostream>
+using namespace std;
 
+int cbinsearch(int*, int, int);
 int main() {
     int arr[] = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5};
     int count = cbinsearch(arr, 15, 3); // count = 3
+    cout << count;
     return 0;
 }

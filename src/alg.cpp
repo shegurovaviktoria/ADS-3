73380da1f2821d7aab8ffe9e20e8447c6b9a06c6
viktoria shegurova <137227112+shegurovaviktoria@users.noreply.github.com>
2024-03-22 23:01:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,12 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  	int count = 0;
+  	for (int i = 0; i < size; i++) 
+  		if (arr[i] == value) 
+  			count += 1;
+  	if (count >= 1)
+  		return count;
+  	else
+      return 0; // если ничего не найдено
 }

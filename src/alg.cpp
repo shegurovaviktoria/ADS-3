a3936136938819f5b643e0f30da01e60a43b3693
viktoria shegurova <137227112+shegurovaviktoria@users.noreply.github.com>
2024-04-01 12:31:32 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int count = 0;
+  for (int i = 0; i < size; i++)
+    if (arr[i] == value)
+      count += 1;
+  if (count >= 1)
+    return count;
+  else;
+    return 0;
 }
